bf76482231d67d7c88238b62199cd895cc62180d
iyxan23 <github@iyxan.dev>
2026-02-11 17:31:10 +0700

    chore(lsp): disable eslint format_on_save

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -41,6 +41,7 @@ return {
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
+        "eslint" -- eslint's format_on_save has abysmal performance
       },
       timeout_ms = 5000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
