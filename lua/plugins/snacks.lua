ea64bcb1fea47e41c3d792ef4b78054f0e0f1677
iyxan23 <github@iyxan.dev>
2026-02-11 17:32:31 +0700

    feat(snacks): add `<leader>fS` to search for symbols

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,12 +1,31 @@
 -- snacks.nvim configuration to replace alpha-nvim in AstroNvim v5
 return {
   "folke/snacks.nvim",
+  dependencies = {
+    {
+      "AstroNvim/astrocore",
+      opts = function(_, opts)
+        local maps = opts.mappings
+        local prefix = "<Leader>"
+
+        maps.n[prefix .. "fS"] = {
+          function() Snacks.picker.lsp_workspace_symbols() end,
+          desc = "Search workspace symbols",
+        }
+      end,
+    },
+  },
   opts = {
     dashboard = {
       enabled = true,
       preset = {
         keys = {
-          { icon = " ", key = ".", desc = "This Session", action = ":lua require('resession').load(vim.fn.getcwd(), { dir = 'dirsession' })" },
+          {
+            icon = " ",
+            key = ".",
+            desc = "This Session",
+            action = ":lua require('resession').load(vim.fn.getcwd(), { dir = 'dirsession' })",
+          },
           { icon = " ", key = "l", desc = "Last Session", action = ":lua require('resession').load \"Last Session\"" },
           { icon = " ", key = "f", desc = "Find Session", action = ":lua require('resession').load()" },
           { icon = " ", key = "w", desc = "Find Word", action = ":lua Snacks.dashboard.pick('live_grep')" },
@@ -33,6 +52,30 @@ return {
         { section = "header" },
         { section = "keys", gap = 1, padding = 1 },
         { section = "startup" },
+      },
+
+      formats = {
+        header = {
+          "%s",
+          -- function(item, tbl)
+          --   local width = tbl.width
+          --
+          --   local header = {}
+          --   local maxWidth = 0
+          --   for _, line in ipairs(vim.split(item.val, "\n")) do
+          --     if #line > maxWidth then maxWidth = #line end
+          --     table.insert(header, line)
+          --   end
+          --
+          --   local prependSpaces = (width / 2) - (maxWidth / 2)
+          --
+          --   for i, line in ipairs(header) do
+          --     header[i] = string.rep(" ", prependSpaces) .. line
+          --   end
+          --
+          --   return table.concat(header, "\n")
+          -- end,
+        },
       },
     },
     -- Configure other snacks modules that replace previous plugins
